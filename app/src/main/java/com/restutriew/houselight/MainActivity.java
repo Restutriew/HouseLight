f8744fee392208fa10131a60b6e20bb3e4d6c78e
Restutriew <restutriew@gmail.com>
2019-09-03 18:19:16 +0700

    Initial commit

---
+++ app/src/main/java/com/restutriew/houselight/MainActivity.java
@@ -0,0 +1,22 @@
+package com.restutriew.houselight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        WebView webview = findViewById(R.id.houselight);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("http://192.168.137.1/project/cikara/cikara-ruhwan/public");
+    }
+}
